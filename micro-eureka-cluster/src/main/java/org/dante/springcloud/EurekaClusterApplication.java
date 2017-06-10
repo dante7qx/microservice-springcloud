38d061a137832b92179a533c54f81c56587b1a93
dante <sunchao.0129@163.com>
2017-06-10 14:55:43 +0800

    添加Eureka Cluster例子

--- micro-eureka-cluster/src/main/java/org/dante/springcloud/EurekaClusterApplication.java
+++ micro-eureka-cluster/src/main/java/org/dante/springcloud/EurekaClusterApplication.java
@@ -6,8 +6,8 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
 @EnableEurekaServer
-public class EurekaStandaloneApplication {
+public class EurekaClusterApplication {
 	public static void main(String[] args) {
-		SpringApplication.run(EurekaStandaloneApplication.class, args);
+		SpringApplication.run(EurekaClusterApplication.class, args);
 	}
 }
--- micro-eureka-cluster/src/test/java/org/dante/springcloud/EurekaClusterApplicationTest.java
+++ micro-eureka-cluster/src/test/java/org/dante/springcloud/EurekaClusterApplicationTest.java
@@ -6,5 +6,5 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class EurekaStandaloneApplicationTest {
+public class EurekaClusterApplicationTest {
 }
