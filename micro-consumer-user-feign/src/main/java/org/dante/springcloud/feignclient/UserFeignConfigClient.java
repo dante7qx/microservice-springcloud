321e8adc81a7be06c1d4a742c80614822bfeb0a6
dante <sunchao.0129@163.com>
2017-06-11 16:23:21 +0800

    add zhi shi

--- micro-consumer-user-feign/src/main/java/org/dante/springcloud/feignclient/UserFeignConfigClient.java
+++ micro-consumer-user-feign/src/main/java/org/dante/springcloud/feignclient/UserFeignConfigClient.java
@@ -9,6 +9,13 @@ import feign.RequestLine;
 
 @FeignClient(name = "micro-eureka-standalone", url="http://localhost:8761/eureka", configuration = UserFeignClientConfig.class)
 public interface UserFeignConfigClient {
+	
+	/**
+	 * Feign 原生注解
+	 * 
+	 * @param serviceid
+	 * @return
+	 */
 	@RequestLine("GET /apps/{serviceid}")
     public String getEurekaInstance(@Param("serviceid") String serviceid);
 }
