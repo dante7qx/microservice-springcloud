c66b5f1dd5a9a365605678421629d06928eea457
dante <sunchao.0129@163.com>
2017-06-12 14:54:16 +0800

    修改相关配置

--- micro-consumer-user-zuul/src/main/java/org/dante/springcloud/config/zuul/ZuulRegexConfig.java
+++ micro-consumer-user-zuul/src/main/java/org/dante/springcloud/config/zuul/ZuulRegexConfig.java
@@ -4,6 +4,15 @@ import org.springframework.cloud.netflix.zuul.filters.discovery.PatternServiceRo
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+/**
+ * 通过正则配置 serviceId 和 zuul 的约定 (只支持服务发现)
+ * 
+ * serviceId: myusers-v1
+ * route: /v1/myusers/**
+ * 
+ * @author dante
+ *
+ */
 //@Configuration
 public class ZuulRegexConfig {
 	@Bean
