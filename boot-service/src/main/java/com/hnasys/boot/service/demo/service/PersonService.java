c8b357a904d5e41326e77a63e3eda0174e345018
dante <sunchao.0129@163.com>
2017-06-25 22:17:16 +0800

    del boot project

--- boot-service/src/main/java/com/hnasys/boot/service/demo/service/PersonService.java
+++
@@ -1,9 +0,0 @@
-package com.hnasys.boot.service.demo.service;
-
-import org.springframework.data.domain.Page;
-
-import com.hnasys.boot.dao.demo.domain.Person;
-
-public interface PersonService {
-	public Page<Person> findPage() throws Exception;
-}
