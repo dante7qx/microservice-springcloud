7947aafcd94c0410540e050737d5901bc2ad4b4c
dante <sunchao.0129@163.com>
2018-03-30 16:14:55 +0800

    Edgware版本特性研究

--- micro-consumer-user-feign/src/main/java/org/dante/springcloud/MicroConsumerUserFeignApplication.java
+++ micro-consumer-user-feign/src/main/java/org/dante/springcloud/MicroConsumerUserFeignApplication.java
@@ -2,11 +2,9 @@ package org.dante.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
 
 @SpringBootApplication
-@EnableEurekaClient
 @EnableFeignClients
 public class MicroConsumerUserFeignApplication {
 	public static void main(String[] args) {
--- micro-eureka-client-exclude-jersey/src/main/java/org/dante/springcloud/EurekaClientExcludeJerseyApplication.java
+++ micro-eureka-client-exclude-jersey/src/main/java/org/dante/springcloud/EurekaClientExcludeJerseyApplication.java
@@ -2,10 +2,8 @@ package org.dante.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
-@EnableEurekaClient
 public class EurekaClientExcludeJerseyApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(EurekaClientExcludeJerseyApplication.class, args);
