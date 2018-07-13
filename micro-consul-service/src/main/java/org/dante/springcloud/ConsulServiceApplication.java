9ad885f3d1968081d9d34d40d889dd5af1ff058e
dante <sunchao.0129@163.com>
2018-07-13 16:02:23 +0800

    集成consul

--- micro-consul-service/src/main/java/org/dante/springcloud/ConsulServiceApplication.java
+++ micro-consul-service/src/main/java/org/dante/springcloud/ConsulServiceApplication.java
@@ -2,10 +2,15 @@ package org.dante.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EurekaClientAutoConfiguration;
 
-@SpringBootApplication
+@EnableDiscoveryClient
+@SpringBootApplication(exclude = { EurekaClientAutoConfiguration.class })
 public class ConsulServiceApplication {
+	
 	public static void main(String[] args) {
 		SpringApplication.run(ConsulServiceApplication.class, args);
 	}
+
 }
