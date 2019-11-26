71aa9b144495501c2930cdae633022855d91aff8
dante <sunchao.0129@163.com>
2019-11-26 11:25:16 +0800

    修改

--- micro-zookeeper-config/src/main/java/org/dante/springcloud/ZookeeperConfigApplication.java
+++ micro-zookeeper-config/src/main/java/org/dante/springcloud/ZookeeperConfigApplication.java
@@ -9,6 +9,13 @@ import org.springframework.cloud.netflix.eureka.EurekaClientAutoConfiguration;
  * https://blog.csdn.net/yuan1991926/article/details/79658015
  * http://www.glmapper.com/2019/01/06/springcloud-config-zk-project/
  * 
+ * 现在Zookeeper中添加znode
+ * 
+ * create /config
+ * create /config/zk
+ * create /config/zk/micro-zookeeper-config,dev
+ * create /config/zk/micro-zookeeper-config,dev/app.name "Hello 世界"
+ * 
  * @author dante
  *
  */
--- micro-zookeeper-config/src/main/java/org/dante/springcloud/controller/HealthController.java
+++ micro-zookeeper-config/src/main/java/org/dante/springcloud/controller/HealthController.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.util.CollectionUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -15,6 +16,7 @@ import org.springframework.web.bind.annotation.RestController;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
+@RefreshScope
 @RestController
 public class HealthController {
 	
